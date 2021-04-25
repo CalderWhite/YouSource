4eddac1bd450cef4c8e7983f52a99dafd5a558af
Calder White <calderwhite1@gmail.com>
2021-04-24 20:49:27 -0400

    Updated so it works again.

--- background.js
+++ background.js
@@ -8,32 +8,59 @@
  * The callback function is called upon completion of the request */
 chrome.runtime.onMessage.addListener(function(request, sender, callback) {
     if (request.action == "xhr") {
-        var xhr = new XMLHttpRequest();
-        var method = request.method ? request.method.toUpperCase() : 'GET';
+        let res = fetch('https://yt1s.com/api/ajaxSearch/index', {
+            method: 'POST',
+            headers: {
+                'authority': 'yt1s.com',
+                'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="90", "Google Chrome";v="90"',
+                'accept': '*\/*',
+                'x-requested-with': 'XMLHttpRequest',
+                'sec-ch-ua-mobile': '?0',
+                'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36',
+                'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                'origin': 'https://yt1s.com',
+                'sec-fetch-site': 'same-origin',
+                'sec-fetch-mode': 'cors',
+                'sec-fetch-dest': 'empty',
+                'referer': 'https://yt1s.com/en5',
+                'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+                'cookie': '__cfduid=dae3087fc6b5a660de9323c890235e84c1619289707; _ga=GA1.2.1357389085.1619289710; _gid=GA1.2.691330434.1619289710; _gat_gtag_UA_173445049_1=1'
+            },
+            body: 'q=' + encodeURI(request.videoURL)
+        }).then(res => res.json())
+          .then(data => {
+            // id
+            let downloads = data.links.mp4;
+            let authKey;
+            if (downloads.hasOwnProperty("22")) {
+                authKey = downloads["22"].k;
+            } else {
+                authKey = downloads[Object.keys(downloads)[0]].k;
+            }
 
-        xhr.onload = function() {
-            callback(xhr.responseText);
-        };
-        xhr.onerror = function() {
-            // Do whatever you want on error. Don't forget to invoke the
-            // callback to clean up the communication port.
-            callback();
-        };
-        xhr.open("GET","http://videomultidownload.com/?act=inforvideo&link=" + request.videoUrl,true);
-        //xhr.setRequestHeader("Cookie"," PHPSESSID=67okcflrmr2e0fe5lctad1etb6; _ga=GA1.2.605565052.1496941977; _gid=GA1.2.1597293982.1496941977; _gat=1");
-        //xhr.setRequestHeader("Origin"," http://videomultidownload.com");
-        //xhr.setRequestHeader("Accept-Encoding"," gzip, deflate");
-        xhr.setRequestHeader("Accept-Language"," en-US,en;q=0.8,ja;q=0.6,zh-CN;q=0.4,zh;q=0.2,fr-CA;q=0.2,fr;q=0.2");
-        //xhr.setRequestHeader("User-Agent"," Mozilla/5.0 (X11; CrOS x86_64 9202.64.2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.146 Safari/537.36");
-        xhr.setRequestHeader("Content-Type"," application/x-www-form-urlencoded; charset=UTF-8");
-        xhr.setRequestHeader("Accept"," *\/*");
-        //xhr.setRequestHeader("Referer"," http://videomultidownload.com/");
-        xhr.setRequestHeader("X-Requested-With"," XMLHttpRequest");
-        //xhr.setRequestHeader("Connection"," keep-alive");
-        if (method == 'POST') {
-            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        }
-        xhr.send(request.data);
+            fetch('https://yt1s.com/api/ajaxConvert/convert', {
+                method: 'POST',
+                headers: {
+                    'authority': 'yt1s.com',
+                    'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="90", "Google Chrome";v="90"',
+                    'accept': '*/*',
+                    'x-requested-with': 'XMLHttpRequest',
+                    'sec-ch-ua-mobile': '?0',
+                    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36',
+                    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                    'origin': 'https://yt1s.com',
+                    'sec-fetch-site': 'same-origin',
+                    'sec-fetch-mode': 'cors',
+                    'sec-fetch-dest': 'empty',
+                    'referer': 'https://yt1s.com/en5',
+                    'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+                    'cookie': '__cfduid=dae3087fc6b5a660de9323c890235e84c1619289707; _ga=GA1.2.1357389085.1619289710; _gid=GA1.2.691330434.1619289710; __atssc=google%3B1; __atuvc=2%7C16; __atuvs=608478a21dad09e9000'
+                },
+                body: 'vid=' + request.videoId + '&k=' + authKey
+            }).then(response => response.json())
+              .then(_data => callback(_data.dlink));
+          })
+
         return true; // prevents the callback from being called too early on return
     }
 });
--- content.js
+++ content.js
@@ -1,28 +1,14 @@
 function main(){
   if(window.location.href.toString().search("v=") >= 0){
+    let searchParams = new URLSearchParams(window.location.search);
     // send a message to a background script, so we aren't limited to https only.
     chrome.runtime.sendMessage({
       method: 'GET',
       action: 'xhr',
-      videoUrl: window.location.href.toString(),
+      videoURL: window.location.href.toString(),
+      videoId: searchParams.get("v"),
       data: ''
-    }, function(responseText) {
-      // parse the text to find the link
-      var parser = new DOMParser();
-      var doc = parser.parseFromString(responseText,"text/html");
-      var resolutions = doc.getElementsByClassName("panel-body")[0].children;
-      var newSource = null;
-      var highestRes = 0;
-      /*
-      for(i=0;i<resolutions.length;i++){
-        res = Number(resolutions[i].text.replace(/\D+/g, ''));
-        if(res > highestRes){
-          highestRes=res;
-          newSource=resolutions[i].href;
-        }
-      }
-      */
-      newSource=resolutions[1].href
+    }, function(newSource) {
       if(newSource === null){
         alert("ERROR FINDING SOURCE TO VIDEO.")
       } else{
